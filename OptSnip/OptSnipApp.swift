5738fce49ed94329d8bbc9cf299afdddd548136e
wen <wen.wa@qq.com>
2024-07-07 14:09:52 +0800

    Initial Commit

---
+++ OptSnip/OptSnipApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptSnipApp.swift
+//  OptSnip
+//
+//  Created by wallace wen on 2024/7/7.
+//
+
+import SwiftUI
+
+@main
+struct OptSnipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
